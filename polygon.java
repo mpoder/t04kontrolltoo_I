d3a41fa7e62341a5d3845f423bed44beeb4d7c74
Mihkel Põder <mpoder@users.noreply.github.com>
2017-02-27 13:23:45 +0200

    Teise punkti lahendus

--- polygon.java
+++ polygon.java
@@ -28,10 +28,23 @@ public class polygon {
 
     public boolean isEquilateral() {
       if (this.isTriangle()) {
-        return true;
+        double calcx1 = (x.get(1) - x.get(0))*(x.get(1) - x.get(0));
+        double calcx2 = (x.get(2) - x.get(1))*(x.get(2) - x.get(1));
+        double calcx3 = (x.get(0) - x.get(2))*(x.get(0) - x.get(2));
+        double calcy1 = (y.get(1) - y.get(0))*(y.get(1) - y.get(0));
+        double calcy2 = (y.get(2) - y.get(1))*(y.get(2) - y.get(1));
+        double calcy3 = (y.get(2) - y.get(0))*(y.get(2) - y.get(0));
+        double len1 = Math.round(Math.sqrt(calcx1+calcy1));
+        double len2 = Math.round(Math.sqrt(calcx2+calcy2));
+        double len3 = Math.round(Math.sqrt(calcx3+calcy3));
+        System.out.println("Debug: len1:" + len1);
+        System.out.println("Debug: len2:" + len2);
+        System.out.println("Debug: len3:" + len3);
+        if (len1 == len2 && len2 == len3) {
+          return true;
       } else {
         return false;
       }
-      
-    }
+    } else {return false;}
+}
 }
--- test1.java
+++ test1.java
@@ -2,28 +2,31 @@ public class test1 {
   public static void main(String[] args) {
     polygon poly1 = new polygon();
     polygon poly2 = new polygon();
-    poly1.addX(12);
-    poly1.addY(116);
-    poly1.addX(32);
-    poly1.addY(94);
-    poly1.addX(56);
-    poly1.addY(78);
-    poly1.addX(73);
-    poly1.addY(31);
+    poly1.addX(0);
+    poly1.addY(0);
+    poly1.addX(2);
+    poly1.addY(3);
+    poly1.addX(4);
+    poly1.addY(0);
     poly2.addX(1);
     poly2.addY(1);
+    poly2.addX(4);
+    poly2.addY(4);
     poly2.addX(2);
     poly2.addY(2);
-    poly2.addX(3);
-    poly2.addY(3);
     System.out.println("First x: " + poly1.askX(0));
-    System.out.println("Fourth y: " + poly1.askY(3));
+    System.out.println("Third y: " + poly1.askY(2));
     if (poly1.isTriangle()) {
       System.out.println("First polygon is a triangle");
+      if (poly1.isEquilateral()) {
+        System.out.println("First polygon is equilateral.");
+      };
     }
     if (poly2.isTriangle()) {
       System.out.println("Second polygon is a triangle");
-      poly2.isEquilateral();
+      if (poly2.isEquilateral()) {
+        System.out.println("Second polygon is equilateral.");
+      };
     }
   }
 }
